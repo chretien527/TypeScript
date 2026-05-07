203649feb6f6ebd52b9688d5f6d60187793e2867
NAMBAJIMANA SANO Chretien <chretiensano@gmail.com>
2026-05-07 21:50:43 +0200

    Add files via upload

---
+++ database.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const debug = require('debug')('app:database');
+
+const connectDB = async() => {
+    try{
+        const dbURI = process.env.MONGODB_URI || (config.has('mongodb.uri') ? config.get('mongo.uri') : 'mongodb://localhost:27017/school-mis');
+        const options = config.has('mongodb.opitons') ? config.get('mongodb.options') : {
+            useNewUrlParser : true,
+            useUnifiedTopology: true
+        };
+
+        await mongoose.connect(dbURI, options);
+        debug(`MongoDB connected: ${dbURI}`);
+    } catch (error){
+        debug('MongoDB connection error:',error);
+        process.exit(1)
+    }
+};
+
+mongoose.connection.on('disconnected', () => {
+    debug('MongoDb disconnected');
+});
+
+mongoose.connection.on('error', (error) => {
+    debug('MongoDb error:',error);
+})
+
+module.exports = connectDB;
