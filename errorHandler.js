259897118d1f2de936e961589104286581307eac
NAMBAJIMANA SANO Chretien <chretiensano@gmail.com>
2026-05-08 22:36:00 +0200

    Add files via upload

---
+++ errorHandler.js
@@ -0,0 +1,27 @@
+const debug = require('debug')('app:error');
+
+const errorHandler = (err,req,next) => {
+    debug("Error:",err);
+
+    if(err.name === 'ValidationError'){
+        return res.status(400).json({
+            message: 'Validation Error',
+            errors: err.details?.map(e => e.message) || [err.message]
+        });
+    }
+
+    if(err.name = 'CastError'){
+        return res.status(400).json({message:'Invalid ID format'});
+    }
+
+    if(err.code === 11000){
+        return res.status(400).json({message:'Duplicate entry detected'});
+    }
+
+    res.status(err.status || 500).json({
+        message:err.message || 'Internal Server error',
+        ...(process.env.NODE_ENV === 'development' && {stack:err.stack})
+    });
+};
+
+module.exports = errorHandler;
