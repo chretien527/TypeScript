0bfa018e8c698ded0b9a864bb78f683b295dfcf9
NAMBAJIMANA SANO Chretien <chretiensano@gmail.com>
2026-05-20 22:27:41 +0200

    Add files via upload

---
+++ types.js
@@ -0,0 +1,9 @@
+"use strict";
+let userName = "John";
+let age = 25;
+let isStudent = true;
+let hobbies = ['reading', 'coding', 'gaming'];
+console.log(userName);
+console.log(age);
+console.log(isStudent);
+console.log(hobbies);
